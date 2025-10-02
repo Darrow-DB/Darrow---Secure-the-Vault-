98ff5490b93f058fdbba6a082edf2d44e6424613
Matthew Ward <warski4@protonmail.com>
2025-10-01 22:45:31 -0400

    homework

---
+++ index.js
@@ -0,0 +1,25 @@
+let code1;
+code1= 7+3
+
+
+let code2;
+code2 =  100 / 2 -10
+
+
+let code3;
+code3 = 4 * 10 - 13 % 4
+
+
+console.log("The vault has been secured. The combination is:")
+
+let codeA;
+codeA = (code1 +"-"+code2 +"-"+code3)
+let codeB;
+codeB = codeA
+
+console.log(codeA);
+console.log(codeB);
+
+
+
+
